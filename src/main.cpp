88df56f38dc43da87853abbe32c7a308bf2dc237
CCGerhardt <caryg1@umbc.edu>
2025-05-29 21:43:04 -0400

    Cleaned up the menu, need to start working on the menu class and rotary encoder interface.

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include "DHT.h"
 #include "Adafruit_GFX.h"
 #include "Adafruit_SSD1306.h"
+#include "RotaryEncoder.h"
 
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
@@ -18,76 +19,104 @@ Adafruit_SSD1306 display(SCREEN_WIDTH,SCREEN_HEIGHT,&Wire, OLED_RESET);
 #define DT 35
 #define SW 34
 
+#define DHTPIN 27
+DHT dht(DHTPIN,DHT11);
+long lastReadTime = 0;
+float temp = dht.readTemperature();
+
 void testScrollText(void);
 void mainMenu(void);
 
-void setup() {
-  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
-  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
-    Serial.println(F("SSD1306 allocation failed"));
-    for(;;); // Don't proceed, loop forever
-  }
+int arrowPos = 0;
 
-  // Show initial display buffer contents on the screen --
-  // the library initializes this with an Adafruit splash screen.
-  display.display();
-  delay(500); // Pause for 0.5 seconds
+RotaryEncoder encoder(CLK, DT, RotaryEncoder::LatchMode::TWO03);
 
-  // Clear the buffer
-  display.clearDisplay();
-  // testScrollText();
-  }
+void setup()
+{
+  Serial.begin(115200);
+  while (! Serial);
+  Serial.println("SimplePollRotator example for the RotaryEncoder library.");
+} // setup()
 
-void loop() {
-  mainMenu();
-}
 
-void mainMenu(void){
-  uint8_t width = 128;
-  uint8_t height = 11;
-  uint8_t x_cord = 0;
-  uint8_t upper_y = 0;
-  uint8_t lower_y = SCREEN_HEIGHT-height;
-  uint8_t numLines = 3;
+// Read the current position of the encoder and print out when changed.
+void loop()
+{
+  static int pos = 0;
+  encoder.tick();
 
-  display.drawRect(x_cord,upper_y,width,height, SSD1306_WHITE);
-  display.setTextSize(1);
-  display.setTextColor(SSD1306_WHITE);
-  display.setCursor(x_cord+2, upper_y+2);
+  int newPos = encoder.getPosition();
+  if (pos != newPos) {
+    Serial.print("pos:");
+    Serial.print(newPos);
+    Serial.print(" dir:");
+    Serial.println((int)(encoder.getDirection()));
+    pos = newPos;
+  } // if
+} // loop ()
 
-  for (uint8_t i = 0; i < numLines;i++){
-    display.drawFastHLine(x_cord,upper_y+height+10*(i+1),width,SSD1306_WHITE);
-  }
+// The End
 
-  display.printf("Test: %.2f%cC\n",(float)13.0,(char)DEGREE);
+// void setup() {
+//   // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+//   if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+//     Serial.println(F("SSD1306 allocation failed"));
+//     for(;;); // Don't proceed, loop forever
+//   }
 
-  // display.drawFastHLine(x_cord,)
-  display.drawRect(x_cord,lower_y,width,height, SSD1306_WHITE);
-  display.display();
-}
-void testScrollText(void) {
-  display.clearDisplay();
+//   // Show initial display buffer contents on the screen --
+//   // the library initializes this with an Adafruit splash screen.
+//   display.display();
+//   delay(500); // Pause for 0.5 seconds
 
-  display.setTextSize(2); // Draw 2X-scale text
-  display.setTextColor(SSD1306_WHITE);
-  display.setCursor(10, 0);
-  display.println(F("scroll"));
-  display.display();      // Show initial text
-  delay(100);
+//   dht.begin();
+//   }
 
-  // Scroll in various directions, pausing in-between:
-  display.startscrollright(0x00, 0x0F);
-  delay(2000);
-  display.stopscroll();
-  delay(1000);
-  display.startscrollleft(0x00, 0x0F);
-  delay(2000);
-  display.stopscroll();
-  delay(1000);
-  display.startscrolldiagright(0x00, 0x07);
-  delay(2000);
-  display.startscrolldiagleft(0x00, 0x07);
-  delay(2000);
-  display.stopscroll();
-  delay(1000);
-}
+// void loop() {
+//   // Clear bufffer and re-write on loop call
+//   display.clearDisplay();
+//   mainMenu(); // Display the main menu with temperature sensor
+//   arrowPos++; // Increment arrow position
+// }
+
+// void mainMenu(void){
+
+//   if (millis()-lastReadTime > 1000){
+//     temp = dht.readTemperature();
+//     lastReadTime = millis();
+//   }
+
+//   /*
+//     Below are pixel art parameters
+//   */
+//   uint8_t width = 128;
+//   uint8_t height = 11;
+//   uint8_t x_cord = 0;
+//   uint8_t upper_y = 0;
+//   uint8_t lower_y = SCREEN_HEIGHT-height;
+//   uint8_t numLines = 4;
+
+//   display.drawRect(x_cord,upper_y,width,height, SSD1306_WHITE);
+//   display.setTextSize(1);
+//   display.setTextColor(SSD1306_WHITE);
+//   display.setCursor(x_cord+2, upper_y+2);
+//   display.printf("Test: %.2f%cC\n",temp,(char)DEGREE);
+
+//   uint8_t y_cord = upper_y+height;
+//   for (uint8_t i = 0; i < numLines;i++){
+//     display.setCursor(x_cord+2, y_cord);
+//     display.printf(" Test: %d\n",(uint8_t)i);
+//     y_cord += 11;
+
+//   }
+
+//   uint8_t arrP = upper_y+height+11*(arrowPos%numLines);
+//   display.setCursor(x_cord+2,arrP);
+//   display.printf(">");
+
+
+
+//   // display.drawFastHLine(x_cord,)
+//   display.drawRect(x_cord,lower_y,width,height, SSD1306_WHITE);
+//   display.display();
+// }
