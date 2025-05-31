147ad8e5ce79424cf0ce36568d2025cc0be284cb
CCGerhardt <caryg1@umbc.edu>
2025-05-30 20:09:02 -0400

    A lot of things fucked up I'm working on it

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,6 @@
 #include "DHT.h"
 #include "Adafruit_GFX.h"
 #include "Adafruit_SSD1306.h"
-#include "RotaryEncoder.h"
 
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
@@ -29,94 +28,66 @@ void mainMenu(void);
 
 int arrowPos = 0;
 
-RotaryEncoder encoder(CLK, DT, RotaryEncoder::LatchMode::TWO03);
+void setup() {
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
 
-void setup()
-{
-  Serial.begin(115200);
-  while (! Serial);
-  Serial.println("SimplePollRotator example for the RotaryEncoder library.");
-} // setup()
+  // Show initial display buffer contents on the screen --
+  // the library initializes this with an Adafruit splash screen.
+  display.display();
+  delay(500); // Pause for 0.5 seconds
 
+  dht.begin();
+  }
 
-// Read the current position of the encoder and print out when changed.
-void loop()
-{
-  static int pos = 0;
-  encoder.tick();
+void loop() {
+  // Clear bufffer and re-write on loop call
+  display.clearDisplay();
+  mainMenu(); // Display the main menu with temperature sensor
+  arrowPos++; // Increment arrow position
+}
 
-  int newPos = encoder.getPosition();
-  if (pos != newPos) {
-    Serial.print("pos:");
-    Serial.print(newPos);
-    Serial.print(" dir:");
-    Serial.println((int)(encoder.getDirection()));
-    pos = newPos;
-  } // if
-} // loop ()
+void mainMenu(void){
 
-// The End
+  if (millis()-lastReadTime > 1000){
+    temp = dht.readTemperature();
+    lastReadTime = millis();
+  }
 
-// void setup() {
-//   // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
-//   if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
-//     Serial.println(F("SSD1306 allocation failed"));
-//     for(;;); // Don't proceed, loop forever
-//   }
+  /*
+    Below are pixel art parameters
+  */
+  uint8_t width = 128;
+  uint8_t height = 11;
+  uint8_t x_cord = 0;
+  uint8_t upper_y = 0;
+  uint8_t lower_y = SCREEN_HEIGHT-height;
+  uint8_t numLines = 4;
 
-//   // Show initial display buffer contents on the screen --
-//   // the library initializes this with an Adafruit splash screen.
-//   display.display();
-//   delay(500); // Pause for 0.5 seconds
+  display.drawRect(x_cord,upper_y,width,height, SSD1306_WHITE);
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(x_cord+2, upper_y+2);
+  display.printf("Test: %.2f%cC\n",temp,(char)DEGREE);
 
-//   dht.begin();
-//   }
+  uint8_t y_cord = upper_y+height;
+  for (uint8_t i = 0; i < numLines;i++){
+    display.setCursor(x_cord+2, y_cord);
+    display.printf(" Test: %d\n",(uint8_t)i);
+    y_cord += 11;
 
-// void loop() {
-//   // Clear bufffer and re-write on loop call
-//   display.clearDisplay();
-//   mainMenu(); // Display the main menu with temperature sensor
-//   arrowPos++; // Increment arrow position
-// }
+  }
 
-// void mainMenu(void){
-
-//   if (millis()-lastReadTime > 1000){
-//     temp = dht.readTemperature();
-//     lastReadTime = millis();
-//   }
-
-//   /*
-//     Below are pixel art parameters
-//   */
-//   uint8_t width = 128;
-//   uint8_t height = 11;
-//   uint8_t x_cord = 0;
-//   uint8_t upper_y = 0;
-//   uint8_t lower_y = SCREEN_HEIGHT-height;
-//   uint8_t numLines = 4;
-
-//   display.drawRect(x_cord,upper_y,width,height, SSD1306_WHITE);
-//   display.setTextSize(1);
-//   display.setTextColor(SSD1306_WHITE);
-//   display.setCursor(x_cord+2, upper_y+2);
-//   display.printf("Test: %.2f%cC\n",temp,(char)DEGREE);
-
-//   uint8_t y_cord = upper_y+height;
-//   for (uint8_t i = 0; i < numLines;i++){
-//     display.setCursor(x_cord+2, y_cord);
-//     display.printf(" Test: %d\n",(uint8_t)i);
-//     y_cord += 11;
-
-//   }
-
-//   uint8_t arrP = upper_y+height+11*(arrowPos%numLines);
-//   display.setCursor(x_cord+2,arrP);
-//   display.printf(">");
+  uint8_t arrP = upper_y+height+11*(arrowPos%numLines);
+  display.setCursor(x_cord+2,arrP);
+  display.printf(">");
 
 
 
-//   // display.drawFastHLine(x_cord,)
-//   display.drawRect(x_cord,lower_y,width,height, SSD1306_WHITE);
-//   display.display();
-// }
+  // display.drawFastHLine(x_cord,)
+  display.drawRect(x_cord,lower_y,width,height, SSD1306_WHITE);
+  display.display();
+}
